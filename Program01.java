af1d4f1b39b9b2fcef3b44a9cac3eb5c9b3e12cf
Anjali Patil <patilanjali2204@gmail.com>
2025-07-16 22:25:36 +0530

    Add files via upload
    using database

---
+++ Program01.java
@@ -0,0 +1,40 @@
+package com.sunbeam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program01 {
+	public static void main(String[] args) {
+		try {
+			// load and register the Driver class towards JVM
+			// This is an optional step from JDBC 4 spec onwards
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dkte_db", "root", "root");
+//			if (connection != null)
+//				System.out.println("Connection is successful");
+//			else
+//				System.out.println("Failed...");
+
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery("SELECT * FROM user");
+			while (rs.next()) {
+				System.out.println(rs.getInt(1) + " - " + rs.getString(2));
+			}
+			statement.close();
+			connection.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
