ede0302889b9308285cc7a6d68e09aaabd22a7a7
Anjali Patil <patilanjali2204@gmail.com>
2025-07-16 22:27:46 +0530

    Update and rename Program01.java to Database.java

--- Database.java
+++ Database.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Program01 {
+public class Database {
 	public static void main(String[] args) {
 		try {
 			// load and register the Driver class towards JVM
