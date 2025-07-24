c14888798c9d864607c8c0ae408fa86287351c7c
Anjali Patil <patilanjali2204@gmail.com>
2025-07-24 23:15:10 +0530

    Add files via upload

---
+++ FileCreate.java
@@ -0,0 +1,18 @@
+package file;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileCreate {
+	public static void main(String[] args) throws Throwable {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter source path:");
+		String src = sc.next();
+		
+		File file = new File(src);
+		boolean sucess = file.createNewFile();
+		System.out.println("Sucess-------");
+	}
+
+}
